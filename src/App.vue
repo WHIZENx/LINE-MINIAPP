4d6d3f123127086a2d74488458f3a7bce3ec44de
Nattapat Pinta <47101447+WHIZENx@users.noreply.github.com>
2021-01-07 15:59:46 +0700

    Initial commit from VC

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <TopToolbar></TopToolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <BottomNav></BottomNav>
+  </v-app>
+</template>
+
+<script>
+import TopToolbar from "@/components/TopToolbar";
+import BottomNav from "@/components/BottomNav";
+export default {
+  components: {
+    TopToolbar,
+    BottomNav,
+  },
+  name: "App",
+  beforeCreate() {
+    console.log("init");
+
+  },
+};
+</script>
---
+++ src/components/BottomNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-bottom-navigation app fixed grow color="primary">
+    <v-btn value="code" :to="{ name: 'News' }">
+      <span>ข่าว</span>
+      <v-icon>mdi-newspaper</v-icon>
+    </v-btn>
+    <v-btn value="favorites" :to="{ name: 'Qrkeep' }">
+      <span>เก็บ</span>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+    <v-btn value="Scan" :to="{ name: 'Scan' }">
+      <span>สแกน</span>
+      <v-icon>mdi-qrcode</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  name: "BottomNav",
+  data: () => ({
+    drawer: null,
+  }),
+};
+</script>
